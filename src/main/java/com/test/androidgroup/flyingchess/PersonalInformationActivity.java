05925fd648f39486b43e262f274b3899aad2d74d
kehan <sysukehan@163.com>
2016-03-26 20:57:19 +0800

    还差改个人资料界面，整个app的数据收集，以及一些界面跳转的衔接问题

--- src/main/java/com/test/androidgroup/flyingchess/PersonalInformationActivity.java
+++ src/main/java/com/test/androidgroup/flyingchess/PersonalInformationActivity.java
@@ -2,14 +2,22 @@ package com.test.androidgroup.flyingchess;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by kehan on 16-3-21.
  */
 public class PersonalInformationActivity extends Activity {
+    private ImageView image;
+    private TextView title;
     @Override
     public void onCreate(Bundle savedInstancestate) {
         super.onCreate(savedInstancestate);
         setContentView(R.layout.information_layout);
+        image = (ImageView) findViewById(R.id.user_image);
+        title = (TextView) findViewById(R.id.title);
+        title.setText("个人资料");
+        image.setImageResource(R.mipmap.config);
     }
 }
